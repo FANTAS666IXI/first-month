7916ba2171ecd92e6dedcf95e405dac8c90cc9ce
FANTAS666IXI <fantas666x@hotmail.com>
2025-10-17 05:39:13 +0200

    FIRST MONTH 0.6.0
    Add Controls Carousel

--- src/Components/Carousel.js
+++ src/Components/Carousel.js
@@ -4,15 +4,21 @@ import translations from "../Data/translations";
 function Carousel() {
   const lang = "en";
   const [day, setDay] = useState(1);
-  const todaySentence = translations[lang].carousel[day];
+  const sentence = translations[lang].carousel[day];
 
   const handlePrev = () => setDay((prev) => (prev > 1 ? prev - 1 : 31));
   const handleNext = () => setDay((prev) => (prev < 31 ? prev + 1 : 1));
 
   return (
     <div className="carousel-wrapper">
-      <div className="day-indicator">
-        Día {day}
+      <div className="header">
+        <div className="day-indicator">Day {day}</div>
+        <div className="controls">
+          <i className="fa-solid fa-arrows-rotate replay" />
+          <button className="play">
+            <i className="fa-solid fa-play" />
+          </button>
+        </div>
       </div>
       <div className="carousel">
         <img src={`images/${day}.png`} alt={`carousel item ${day}`} />
@@ -21,7 +27,7 @@ function Carousel() {
         <button className="arrow left" onClick={handlePrev}>
           <i className="fa-solid fa-chevron-left" />
         </button>
-        <p>{todaySentence}</p>
+        <p>{sentence}</p>
         <button className="arrow right" onClick={handleNext}>
           <i className="fa-solid fa-chevron-right" />
         </button>
